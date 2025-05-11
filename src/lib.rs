381c0113b6a2fd719c52f9f30d6260dc134450c7
Seanghay Yath <seanghay.dev@gmail.com>
2025-05-11 22:52:47 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
 pub fn optimize(svg: String) -> String {
     let tree = usvg::Tree::from_str(&svg, &usvg::Options::default()).unwrap();
     tree.to_string(&usvg::WriteOptions::default())
